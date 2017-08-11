76a393b1ee0232ab321a97b431fe5bfda6abc204
Kang Seongchan <zelatool@gmail.com>
2017-08-11 16:07:09 +0900

    feature/seongchan 8/11

--- EClass/EClass/DetailTableViewController.swift
+++ EClass/EClass/DetailTableViewController.swift
@@ -13,7 +13,7 @@ class DetailTableViewController: UIViewController {
     @IBOutlet weak var myTableView: UITableView!
 
     var myData = LectureGenerator.getLecture()
-    
+    var myLectureData:[UIImage] = [#imageLiteral(resourceName: "pac-man-logo.gif"), #imageLiteral(resourceName: "default-user-image"),#imageLiteral(resourceName: "whiteStar")]
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -46,7 +46,7 @@ class DetailTableViewController: UIViewController {
 
 }
 
-extension DetailTableViewController:UITableViewDelegate, UITableViewDataSource {
+extension DetailTableViewController:UITableViewDelegate, UITableViewDataSource, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 7
     }
@@ -57,6 +57,7 @@ extension DetailTableViewController:UITableViewDelegate, UITableViewDataSource {
             let cell = tableView.dequeueReusableCell(withIdentifier: "MainImageCell", for: indexPath) as! SimpleImageTableViewCell
             
             cell.settingMyImage(#imageLiteral(resourceName: "pac-man-logo.gif"))
+            cell.selectionStyle = .none
             
             return cell
             
@@ -64,14 +65,17 @@ extension DetailTableViewController:UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "IntroductionCell", for: indexPath) as! IntroductionTableViewCell
             
             cell.setLectureInfo("팩맨", "5.0", "서울 강서구 공항오피스텔", "4명", "50000원", "주 2회", "총 8회 16시간")
+            cell.selectionStyle = .none
+
         
             return cell}
         
         else if indexPath.row == 2{
             let cell = tableView.dequeueReusableCell(withIdentifier: "tutorInfoCell", for: indexPath) as! TutorInfoTableViewCell
             
-            cell.setTutor(#imageLiteral(resourceName: "pac-man-logo.gif"), "성찬", tutorComment: "하이하이 \n 이거 많이 쓰면 늘어나는거 팩트입니까? 항ㄴ훈이훈이후니우히나위 저는 IOS 개발자인데 팩맨 겁나 잘하구여 그 똥망한 영화 픽셀도 개즐겁게 본 진성 너드입니다")
-            
+            cell.setTutor(#imageLiteral(resourceName: "default-user-image"), "성찬", tutorComment: "하이하이 \n 이거 많이 쓰면 늘어나는거 팩트입니까? 항ㄴ훈이훈이후니우히나위 저는 IOS 개발자인데 팩맨 겁나 잘하구여 그 똥망한 영화 픽셀도 개즐겁게 본 진성 너드입니다")
+            cell.selectionStyle = .none
+
             
             return cell
         
@@ -80,23 +84,27 @@ extension DetailTableViewController:UITableViewDelegate, UITableViewDataSource {
             
     cell.tutorBasicInfo.text = "인하대학교"
     cell.tutorDetailInfo.text = "jsakdnaksjfbldsakfbsdkfblasdkjfbadsjkbflakjdsfbadsjkfbasjdkfbsjdk\n bfljksdfbasdjkfbadjskfbdkasfbjkdasfblajkdsfbjaldsfbl\n aksdjfbsajldkfbalsjdkfblasdjkfblasdkjfbaslkdfbaljkdsfbald\n sjfblksdjafbaksd\n fblasdkfblaskdjfblaskjdfbalsjdfbasl\n jdfblasdjkfbadlsjkfbladskfbasldjkfblasdjfb\n jadbflj"
-            
+            cell.selectionStyle = .none
+
             return cell
             
         }else if indexPath.row == 4{
             let cell = tableView.dequeueReusableCell(withIdentifier: "LectureIntroTableViewCell", for: indexPath) as! LectureIntroTableViewCell
-            
+            cell.selectionStyle = .none
+
             cell.setValues(myData[indexPath.row - 4])
             
             return cell
         }else if indexPath.row == 5{
             let cell = tableView.dequeueReusableCell(withIdentifier: "MapLocationTableViewCell", for: indexPath) as! MapLocationTableViewCell
             
-            
+            cell.selectionStyle = .none
+
             return cell
         }else if indexPath.row == 6{
             let cell = tableView.dequeueReusableCell(withIdentifier: "LectureReviewTableViewCell", for: indexPath) as! LectureReviewTableViewCell
-            
+            cell.selectionStyle = .none
+
             return cell
         }
         
@@ -119,6 +127,51 @@ extension DetailTableViewController:UITableViewDelegate, UITableViewDataSource {
         print(indexPath)
         
     }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        
+        if section == 0 {
+        
+        let layout = UICollectionViewFlowLayout()
+
+        let headerView = UICollectionView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.width), collectionViewLayout: layout)
+        
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = UIEdgeInsetsMake(0, 0, 0, 0)
+//        layout.itemSize = CGSize.init(width: (headerView.window?.bounds.width)!, height: (headerView.window?.bounds.height)!)
+        headerView.isPagingEnabled = true
+        headerView.register(UINib.init(nibName: "LectureImageCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "LectureImageCollectionViewCell")
+        
+        headerView.delegate = self
+        headerView.dataSource = self
+    
+            return headerView}
+        
+        return UIView()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 180
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return myLectureData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "LectureImageCollectionViewCell", for: indexPath) as! LectureImageCollectionViewCell
+        
+        cell.lectureImage.image = myLectureData[indexPath.item]
+        
+        
+        
+        return cell
+    }
+    
+    
+    
 //    
 //    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
 //        if section == 0 {
